08defba160998745be3c0fb6b28f2831970a3ea5
George Duffett <george.duffett@me.com>
2020-02-04 00:31:37 +0000

    Better coord plotting

--- calc_path.py
+++ calc_path.py
@@ -3,19 +3,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Initialize elements
-L = ope.lens(10)
 fs = ope.free_space(10)
-ray = ope.ray(0, np.pi*10/180)
+L1 = ope.lens(10)
+L2 = ope.lens(20)
+
+ray_1 = ope.ray(3, np.pi*10/180)
+ray_2 = ope.ray(-3, -np.pi*10/180)
+ray_3 = ope.ray(0, 0)
+
+rays = [ray_1, ray_2, ray_3]
 
 # Create optical path
-P = ope.path(fs, L, fs, L, fs)
+P = ope.path(fs, L1, fs, L2, fs, fs)
 
 # Calculate coordinates on optical path
-(x_coords, y_coords) = P.calc_coords(ray)
-
-# Plot coordinates
 fig, ax = plt.subplots()
-ax.plot(x_coords ,y_coords)
+
+P.plot_lenses(ax)
+
+# Calculate and plot coordinates
+coords_list = [(P.calc_coords(r)) for r in rays]
+for x, y in coords_list: ax.plot(x, y, color='C0')
+
 ax.grid(True)
 ax.set_aspect('equal')
 plt.show()
