dfc2853903625c206d266c961a0829d4cc6fef49
George Duffett <george.duffett@me.com>
2020-02-04 20:00:24 +0000

    Added multi-ray example

--- multi_ray_example.py
+++ multi_ray_example.py
@@ -5,25 +5,21 @@ import numpy as np
 # Initialize elements
 fs = ope.free_space(10)
 L1 = ope.lens(10)
-L2 = ope.lens(20)
+L2 = ope.lens(10)
 
-ray_1 = ope.ray(3, np.pi*10/180)
-ray_2 = ope.ray(-3, -np.pi*10/180)
-ray_3 = ope.ray(0, 0)
-
-rays = [ray_1, ray_2, ray_3]
+# Define rays
+rays = [ope.ray(0, np.pi*th/180) for th in np.linspace(-10, 10, 10)]
 
 # Create optical path
 P = ope.path(fs, L1, fs, L2, fs, fs)
 
-# Calculate coordinates on optical path
+# Calculate ray coordinates along the path, P
+coords_list = [P.calc_coords(r) for r in rays]
+
+# Plot rays and lenses
 fig, ax = plt.subplots()
-
-P.plot_lenses(ax)
-
-# Calculate and plot coordinates
-coords_list = [(P.calc_coords(r)) for r in rays]
 for x, y in coords_list: ax.plot(x, y, color='C0')
+P.plot_lenses(ax)
 
 ax.grid(True)
 ax.set_aspect('equal')
