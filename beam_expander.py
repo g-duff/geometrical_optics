b9520e6e0d1b1232a0453e4f7d70a061eb99487b
George Duffett <george.duffett@me.com>
2020-02-04 20:06:12 +0000

    Replaced multi_ray with more meaningful beam_expander example

--- beam_expander.py
+++ beam_expander.py
@@ -2,16 +2,17 @@ import OpticalElements as ope
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Initialize elements
-fs = ope.free_space(10)
-L1 = ope.lens(10)
-L2 = ope.lens(10)
+# Define point source
+rays = [ope.ray(th, 0) for th in np.linspace(-10, 10, 10)]
 
-# Define rays
-rays = [ope.ray(0, np.pi*th/180) for th in np.linspace(-10, 10, 10)]
+# Initialize elements
+fs1 = ope.free_space(10)
+fs2 = ope.free_space(20)
+L1 = ope.lens(10)
+L2 = ope.lens(20)
 
 # Create optical path
-P = ope.path(fs, L1, fs, L2, fs, fs)
+P = ope.path(fs1, L1, fs1, fs2, L2, fs2)
 
 # Calculate ray coordinates along the path, P
 coords_list = [P.calc_coords(r) for r in rays]
