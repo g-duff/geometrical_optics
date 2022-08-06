20df315ba1180c8ae1a116c7b77879c7d1a4dcfb
george <george.duffett@icloud.com>
2022-08-07 00:48:43 +0100

    Test free space element

--- test/test_optical_element.py
+++ test/test_optical_element.py
@@ -1,6 +1,6 @@
 import unittest
-from src.optical_element import IOpticalElement
-from numpy import array
+from src.optical_element import IOpticalElement, FreeSpace
+from numpy import array, array_equal
 
 
 class TestIOpticalElement(unittest.TestCase):
@@ -40,3 +40,26 @@ class CompleteImplementation(IOpticalElement):
 
 class IncompleteImplementation(IOpticalElement):
     pass
+
+
+class TestFreeSpace(unittest.TestCase):
+
+    def test_is_optical_element(self):
+        # Given
+        # When
+        # Then
+        assert issubclass(FreeSpace, IOpticalElement)
+
+    def test_matrix(self):
+        # Given
+        length = 10
+        free_space = FreeSpace(length)
+
+        expected_free_space_matrix = array([[1, length], [0, 1]])
+
+        # When
+        actual_free_space_matrix = free_space.matrix()
+
+        # Then
+        assert array_equal(expected_free_space_matrix,
+                           actual_free_space_matrix)
