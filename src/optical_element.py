bc1c2c7110b7cde91504247a9932f8358df00ed4
george <george.duffett@icloud.com>
2022-08-07 00:40:58 +0100

    Define free space element

--- src/optical_element.py
+++ src/optical_element.py
@@ -6,3 +6,14 @@ class IOpticalElement(ABC):
     @abstractmethod
     def matrix(self) -> array:
         pass
+
+
+class FreeSpace(IOpticalElement):
+    def __init__(self, length: float):
+        ''' Free space
+
+        length: length of the free space'''
+        self.length = length
+
+    def matrix(self) -> array:
+        return array([[1, self.length], [0, 1]])
