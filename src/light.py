424d2deff40d7e7ffdad0fc8a21be96cde8f2fdf
george <george.duffett@icloud.com>
2022-08-07 12:28:00 +0100

    Add ray

--- src/light.py
+++ src/light.py
@@ -7,3 +7,15 @@ class ILight(ABC):
     def vectors() -> array:
         pass
 
+
+class Ray(ILight):
+    def __init__(self, height_above_optical_axis: float, angle_to_optical_axis: float):
+        ''' A lens
+
+        height_above_optical_axis
+        angle_to_optical_axis: in radians'''
+        self.height = height_above_optical_axis
+        self.theta = angle_to_optical_axis
+
+    def vectors(self) -> array:
+        return array([self.height_above_optical_axis, self.angle_to_optic_axis])
